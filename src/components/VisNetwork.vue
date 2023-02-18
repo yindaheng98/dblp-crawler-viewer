64fb398c79dffc5c3f8819d6e6d00ba4cedee60d
yindaheng98 <yindaheng98@gmail.com>
2023-02-18 16:00:03 +0800

    show network

---
+++ src/components/VisNetwork.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, ref } from "vue";
+import { DataSet } from "vis-data/esnext";
+import { Network } from "vis-network/esnext";
+
+const props = defineProps<{ nodes: object[], edges: object[], options: object }>();
+const visualization = ref(null);
+let network = null;
+onMounted(() => {
+    network = new Network(visualization.value, {
+        nodes: new DataSet(props.nodes),
+        edges: new DataSet(props.edges),
+    }, props.options);
+})
+onBeforeUnmount(() => {
+    if (network !== null)
+        network.destroy();
+})
+</script>
+
+<template>
+    <div ref="visualization">Cannot show vis-network</div>
+</template>
