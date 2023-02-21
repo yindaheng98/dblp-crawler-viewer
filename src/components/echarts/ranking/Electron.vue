1b48dda73b11212e19ac1a1a0c111a55108abe3b
yindaheng98 <yindaheng98@gmail.com>
2023-02-21 22:54:43 +0800

    echarts ok

--- src/components/echarts/ranking/Electron.vue
+++ src/components/echarts/ranking/Electron.vue
@@ -1,24 +1,77 @@
 <script setup lang="ts">
-import { onMounted, reactive } from 'vue';
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { BarChart } from "echarts/charts";
+import {
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+    GridComponent,
+    DataZoomComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { onMounted, ref, provide } from 'vue';
 import { getRankingData, getRankingData_byAllPublications, onUpdate } from '../../../api/electron'
+
+use([
+    CanvasRenderer,
+    BarChart,
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+    GridComponent,
+    DataZoomComponent,
+]);
+
+provide(THEME_KEY, "dark");
 
 const props = defineProps<{ byAllPublications: boolean }>();
 
-const data = reactive({
-    option: {}
+const option = ref({
+    title: {
+        text: 'Ranking'
+    },
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    grid: {
+        left: '20%',
+    },
+    xAxis: { type: 'value' },
+    yAxis: {
+        type: 'category',
+        data: ['No data']
+    },
+    dataZoom: [
+        {
+            type: 'inside',
+            yAxisIndex: 0,
+            filterMode: 'none'
+        }
+    ],
+    series: [
+        {
+            type: 'bar',
+            data: [{ value: 0, itemStyle: { color: "red" } }]
+        },
+    ]
 })
 
-
 async function update() {
+    let data = { id: [0], label: ['No data'], data: [{ value: 1, itemStyle: { color: "red" } }] }
     if (props.byAllPublications)
-        data.option = await getRankingData_byAllPublications()
-    else data.option = await getRankingData()
-    console.log(data)
+        data = await getRankingData_byAllPublications()
+    else data = await getRankingData()
+    option.value.yAxis.data = data.label
+    option.value.series[0].data = data.data
 }
 onMounted(update)
 onUpdate(update)
 </script>
 
 <template>
-    {{ data }}
+    <VChart :option="option" />
 </template>
